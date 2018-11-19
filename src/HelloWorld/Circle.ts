43a2c92705deea42fe9677a4ddb9bf331ee3caad
犹骨 <yingnan.tyn@antfin.com>
2018-11-19 10:01:21 +0800

    Update: 更新loading

--- src/HelloWorld/Circle.ts
+++ src/HelloWorld/Circle.ts
@@ -14,12 +14,24 @@ export default class Arc extends Animation {
   y = 0;
   raduis = 0;
   index = 0;
+  rW = 0;
+  rH = 0;
 
   run = (v: any) => {
+    if(!this.rW || !this.rH) {
+      this.rW = v.width;
+      this.rH = v.height;
+    }
+
     const space = this.index * Math.PI / 180;
     v.left = this.x + this.raduis * Math.cos(space) - v.width / 2;
     v.top = this.y + this.raduis * Math.sin(space) - v.height / 2;
-    this.index = this.index + 1 + (4 - Math.round(((this.index + 90) % 360) / 120));
+
+    const x = (((this.index + 90) % 360) / 360);
+    const off = Math.floor(((- x * x) + x) * 22);
+    this.index = this.index + 1 + off;
+    v.height = v.width = this.rW * (1 + off / 10);
+
 
     if(this.index < 360) return true;
     return false;
--- src/HelloWorld/Loading.ts
+++ src/HelloWorld/Loading.ts
@@ -15,10 +15,12 @@ export class Loading extends river.Activity {
     const arr: any[] = [];
     const arcs: any[] = [];
     const r = this.width / 2;
+    let lastColor = null;
     for (let i = 0; i < size; i += 1) {
-      const rgb = colorTrend(i / 7);
-
-      const circ = new Circle(12, 12, `rgb(${rgb[0]},${rgb[1]},${rgb[2]})`);
+      const rgb = colorTrend(i / size);
+      const nowColor = `rgb(${rgb[0]},${rgb[1]},${rgb[2]})`;
+      const circ = new Circle(14, 14, nowColor, lastColor || nowColor);
+      lastColor = nowColor;
       circ.left = this.width;
       circ.top = this.height;
       const arc = new Arc(r,r, r - circ.width / 2);
@@ -39,17 +41,38 @@ export class Loading extends river.Activity {
 }
 
 export class Circle extends river.View {
-  color: string;
-  constructor(w: number, h: number, color: string) {
+  color1: string;
+  color2: string | undefined;
+  constructor(w: number, h: number, color1: string, color2?: string) {
     super();
     this.width = w;
     this.height = h;
-    this.color = color;
+    this.color1 = color1;
+    this.color2 = color2;
+  }
+
+  colorTrend() {
+    if(this.container) {
+      const x = this.container.width / 2;
+      const l = this.left - x;
+      const t = this.top - x;
+      if(l > 0 && t > 0) return [0,this.height,this.width,0];
+      if(l < 0 && t < 0) return [this.width,0,0,this.height];
+      if(l < 0 && t > 0) return [0,0,this.width,this.height];
+      if(l > 0 && t < 0) return [this.width,this.height,0,0];
+    }
+    return [0,0,this.width,this.height];
   }
 
   onDraw = (ctx: CanvasRenderingContext2D) => {
-    ctx.fillStyle = this.color;
-    ctx.strokeStyle = this.color;
+    const trend = this.colorTrend();
+    const lg = ctx.createLinearGradient(trend[0],trend[1],trend[2],trend[3]);
+
+    lg.addColorStop(0, this.color2 || this.color1);
+    lg.addColorStop(1, this.color1);
+
+    ctx.fillStyle = lg;
+    ctx.strokeStyle = lg;
     ctx.beginPath();
     ctx.arc(this.width / 2, this.height / 2, this.width / 2 - 2, 0, 2 * Math.PI, true);
     ctx.closePath();
